8f049c650538abcecfa3fcac088661f84f88a6a9
peng-huang-ch <h499871809@gmail.com>
2025-04-17 17:00:37 +0800

    Refactor token_metadata module imports to simplify usage of TokenMetadata

--- program/src/extension/token_metadata/mod.rs
+++ program/src/extension/token_metadata/mod.rs
@@ -1,7 +1,5 @@
-use {
-    crate::extension::{Extension, ExtensionType},
-    spl_token_metadata_interface::state::TokenMetadata,
-};
+use crate::extension::{Extension, ExtensionType};
+pub use spl_token_metadata_interface::state::TokenMetadata;
 
 /// Instruction processor for the `TokenMetadata` extension
 pub mod processor;
